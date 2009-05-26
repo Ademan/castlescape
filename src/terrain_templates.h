042d16adbb7bea9ecc9da73929867f4bb17eecb2
Daniel Roberts <Ademan555@gmail.com>
2009-05-25 21:01:53 -0700

    Fixed some of the accessors, made the general normal generation algorithm used.

--- src/terrain_templates.h
+++ src/terrain_templates.h
@@ -9,13 +9,15 @@ struct gl_index_t
     static const unsigned int gl_type = 0xDEADBEEF;
 };
 
-template <typename T>
+template <typename T, typename index_t>
 struct vertex_processor
 {
     static void generate(T & v, const int x, const int y,
                          float height);
     static void postprocess(T * vertices,
-                            const size_t width, const size_t height);
+                            const size_t vertex_count,
+                            index_t * indices,
+                            const size_t index_count);
     static void prepare();
     static void submit(T * vertices);
     static void done();
--- src/terrain_vertex.cpp
+++ src/terrain_vertex.cpp
@@ -12,30 +12,36 @@ vec2 norm2(const float height1, const float height2, const float width)
     result[0] = height / hyp;
     result[1] = width / hyp;
 
-    //return cml::normalize(result);
     return result;
 }
 
 vec2 norm3(const float height1,
+           const float here,
            const float height2,
-           const float height3,
            const float width)
 {
 
-    vec2 left = norm2(height1, height2, width);
-    vec2 right = norm2(height2, height3, width);
+    vec2 left = norm2(height1, here, width);
+    vec2 right = norm2(here, height2, width);
 
-    //return cml::normalize(left + right);
     return left + right;
 }
 
+//FIXME: !!
 vec3 norm_vertex(const float here,
                  const float north, const float south,
                  const float east, const float west,
+                 /*const float ne, const float nw,
+                 const float se, const float sw,*/
                  const float width)
 {
     vec2 vertical = norm3(north, here, south, width);
     vec2 horizontal = norm3(east, here, west, width);
+
+    float hyp = sqrt(2 * width * width);
+
+    /*vec2 up_right = norm3(sw, here, ne, hyp);
+    vec2 up_left = norm3(se, here, nw, hyp);*/
 
     return cml::normalize(vec3(horizontal[0], horizontal[1], 0) +
                           vec3(0, vertical[1], vertical[0]));
